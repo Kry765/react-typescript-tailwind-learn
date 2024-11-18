98dbd5ea1801c49b49670131af6da91607fcf922
Kry765 <krzysztofkleka91@gmail.com>
2024-11-18 21:10:55 +0100

    add new file Link

---
+++ src/scenes/Link.tsx
@@ -0,0 +1,23 @@
+import AnchorLink from "react-anchor-link-smooth-scroll"
+
+type Props = {
+    page: string;
+}
+
+const Link = ({
+    page,
+}: Props) => {
+    const lowerCasePage 
+  return (
+
+    <AnchorLink href={`home`} onClick={} className={}>
+
+    {page}
+
+    </AnchorLink>
+  )
+  
+}
+
+
+export default Link
